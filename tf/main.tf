322fe31c8c1f7dce21c40604e7e001b9a91e79e9
shachar2000 <shacharavraham123@gmail.com>
2025-05-07 22:45:22 +0300

    fix playbook

--- tf/main.tf
+++ tf/main.tf
@@ -365,8 +365,8 @@ resource "aws_security_group" "prometheus_grafana_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    cidr_blocks = var.sg_egress_cidr_blocks
   }
 }
 
